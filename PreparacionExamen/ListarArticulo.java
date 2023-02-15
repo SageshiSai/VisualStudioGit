acd35db74aa6617445d8cb8861243562767a09d3
SageshiSai <saikonogameryt@gmail.com>
2023-02-15 22:01:04 +0100

    Continuamos

--- PreparacionExamen/ListarArticulo.java
+++ PreparacionExamen/ListarArticulo.java
@@ -8,8 +8,8 @@ public class ListarArticulo {
     public void filldata(){
         lista.add(new Refresco("c1","CocaCola","CocaCola",1.50,400,"Dulce",false,true,100));
         lista.add(new Refresco("f2","Fanta","CocaCola",1.50,400,"Dulce",false,true,50));
-        lista.add(new Refresco("a3","Aquarius","CocaCola",1.50,400,"Dulce",false,true,24));
-
+        lista.add(new Refresco("a3","Aquarius","CocaCola",1.50,50,"Dulce",false,true,24));
+        lista.add(new VIno("l1", "LaGranMentira", "LaVerdad", 4, 400, "tinto", "rioja", 1700, "Mentirius"));
     }
 
     public ArrayList<Articulo> reponer(){
