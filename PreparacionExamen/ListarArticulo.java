9ee2006b796436ebef96c3f756b0c444af1b4816
SageshiSai <saikonogameryt@gmail.com>
2023-02-15 22:26:11 +0100

    h2

--- PreparacionExamen/ListarArticulo.java
+++ PreparacionExamen/ListarArticulo.java
@@ -10,16 +10,47 @@ public class ListarArticulo {
         lista.add(new Refresco("f2","Fanta","CocaCola",1.50,400,"Dulce",false,true,50));
         lista.add(new Refresco("a3","Aquarius","CocaCola",1.50,50,"Dulce",false,true,24));
         lista.add(new VIno("l1", "LaGranMentira", "LaVerdad", 4, 400, "tinto", "rioja", 1700, "Mentirius"));
+        lista.add(new VIno("l2", "LaGranVerdad", "LosMentirosos", 4, 400, "tinto", "rioja", 1700, "vartia"));
+        lista.add(new VIno("l3", "Esperanza", "Desesperanzados", 4, 400, "tinto", "rioja", 1700, "esprira"));
+
     }
 
     public ArrayList<Articulo> reponer(){
-        return reponer();
+        ArrayList<Articulo> ListaStock = new ArrayList<>();
+        for(Articulo a : lista){
+            
+            if(a.stock < 50){
+                ListaStock.add(a);
+            }
+            
+        }
+        return ListaStock;
     }
     public ArrayList<Articulo> mascaro(){
-        return mascaro();
+        ArrayList<Articulo> ListaCaro = new ArrayList<>();
+        double max = 0;
+        for(Articulo a : lista){
+            if(max < a.precio){
+                max = a.precio;
+            }
+        }
+        for(Articulo a : lista){
+            if(max == a.precio){
+                ListaCaro.add(a);
+            }
+        }
+
+        return ListaCaro;
     }
     public ArrayList<Articulo> equivalente(String Codigo){
-        return equivalente(Codigo);
+        ArrayList<Articulo> ListaMismoPrecio = new ArrayList<>();
+        double mismo = precio(Codigo);
+        for(Articulo a : lista){
+            if(a.getPrecio() == mismo){
+                ListaMismoPrecio.add(a);
+            }
+        }
+        return ListaMismoPrecio;
     }
     public double precio(String Codigo){
         return precio(Codigo);
