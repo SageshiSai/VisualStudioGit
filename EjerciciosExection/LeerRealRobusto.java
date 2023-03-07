5bada57e14021a15c11a675821bf4dc822cea6e9
SageshiSai <saikonogameryt@gmail.com>
2023-03-07 14:56:34 +0100

    vfdvfds

--- EjerciciosExection/LeerRealRobusto.java
+++ EjerciciosExection/LeerRealRobusto.java
@@ -11,32 +11,32 @@ public class LeerRealRobusto {
         char caracter;
         Scanner teclado = new Scanner(System.in);
         numString = teclado.nextLine();
-        boolean puntoComa =false;
-        
+        boolean puntoComa = false;
+
         try {
             numDouble = Double.parseDouble(numString);
-            System.out.println("El valor de la variable es de "+ numDouble);
-            
+            System.out.println("El valor de la variable es de " + numDouble);
+
         } catch (NumberFormatException e) {
             System.out.println("No has agregado correctamente los digitos validos. Se corregira automaticamente");
-            
+
             for (int i = 0; i < numString.length(); i++) {
                 caracter = numString.charAt(i);
 
-            if (Character.isDigit(caracter)) {
+                if (Character.isDigit(caracter)) {
                     valorCorregido += caracter;
-            } else if((caracter == '.' || caracter == ',') && !puntoComa) {
+                } else if ((caracter == '.' || caracter == ',') && !puntoComa) {
                     valorCorregido += '.';
                     puntoComa = true;
+                }
+
             }
-            
+            if (valorCorregido.length() > 0) {
+                numDouble = Double.parseDouble(valorCorregido);
+            }
+
         }
-        if (valorCorregido.length()>0) {
-            numDouble = Double.parseDouble(valorCorregido);
-        }
-        
-    }
-    System.out.println("El valor de la variable es de "+ numDouble);
+        System.out.println("El valor de la variable es de " + numDouble);
         teclado.close();
-}
+    }
 }
